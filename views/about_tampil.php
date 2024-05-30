d8edd1d05a44f3826f5f753022bccfc2a40d9e70
unknown <alexisronauli@gmail.com>
2024-05-30 14:08:24 +0700

    second commit

---
+++ views/about_tampil.php
@@ -0,0 +1,45 @@
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12">
+            <div class="panel panel-success">
+                <div class="panel-heading">
+                    <h3 class="panel-title"><span class="fa fa-user-plus"></span> About</h3>
+                </div>
+                <div class="panel-body">
+                    <table id="deskripsi" class="table table-bordered table-striped table-hover">
+                        <thead>
+                            <p align="center"><img src="img/zahira.png"></img></p>
+                            <label class="col-sm-12 control-label"><center><strong>SISTEM INFORMASI PERPUSTAKAAN </strong></center></label>
+                        </thead>
+                        <tbody>
+                          <div class="col-sm-2" align="center">
+
+                          </div>
+                          <div class="col-sm-8" align="center">
+                            <br>
+                            Sistem Informasi Perpustakaan
+                            <br>
+                            <br>
+                            Teknik Informatika Universitas Lampung
+                            <br>
+                            Nama : Alexis Ronauli Manurung
+                            <br>
+                            NIM  : 2215061109
+                            <br>
+                            Nama : Putu Dewi Andriani
+                            <br>
+                            NIM  : 2215061033
+                            <br>
+                            Kelas: Pemrograman Web
+                            <br>
+                          </div>
+                          <div class="col-sm-2" align="center">
+                          </div>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
